c3ed7081334a32c048a6c317299ea6e0345fbcc7
duyvang <ndvang264@gmail.com>
2021-09-05 21:20:22 +0700

    first commit

--- src/test/java/usermanager/ApplicationTests.java
+++ src/test/java/usermanager/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bezkoder.springjwt;
+package usermanager;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootSecurityJwtApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
